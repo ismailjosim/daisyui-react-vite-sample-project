3d1a39c374a0c041c9a20400c7a5b8648869c55b
ismailjosim <75038630+ismailjosim@users.noreply.github.com>
2023-06-01 14:45:08 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-vite-sample-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-vite-65xdkd)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header/Header';
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
